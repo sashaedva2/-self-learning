f002555e4bab1655e4734a4783ba88a6bc892dfe
Aleksandr <sashaedva@mail,ru>
2020-04-19 23:36:29 +0300

    Вычислили расстояния между городами

--- lesson2/00_distance.py
+++ lesson2/00_distance.py
@@ -12,8 +12,13 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
-
+distances = dict()
+moscow=sites['Moscow']
+london=sites['London']
+paris=sites['Paris']
+moscow_london= ((moscow[1] - london[1]) ** 2 + (moscow[2] - london[2]) ** 2) ** 0.5
+moscow_paris=((moscow[1] - paris[1]) ** 2 + (moscow[2] - paris[2]) ** 2) ** 0.5
+london_paris=((paris[1] - london[1]) ** 2 + (paris[2] - london[2]) ** 2) ** 0.5
 # TODO здесь заполнение словаря
 
 print(distances)
