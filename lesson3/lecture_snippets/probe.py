22951e11d831aa8a166cc578952a777bde1535a3
Aleksandr <sashaedva@mail,ru>
2020-04-20 22:19:08 +0300

    Отладка level up

--- lesson3/lecture_snippets/probe.py
+++ lesson3/lecture_snippets/probe.py
@@ -1,5 +1,11 @@
 print('дратути!')
-x = 34
-y = 43
-print(x * y)
+my_family_height = [
+    ['мама', 168],
+    ['папа', 186],
+    ['я', 200],
+]
+total_height = my_family_height[0][1]
+total_height += my_family_height[1][1]
+total_height += my_family_height[2][1]
+print('Общий рост моей семьи', total_height)
 print('дотвидания!')
