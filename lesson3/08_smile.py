dbe601369ea3cb109c90abbb350f359e2a7dec6e
Aleksandr <sashaedva@mail,ru>
2020-04-23 01:11:45 +0300

    Сгенерирована функция для рисования смайликов)

--- lesson3/08_smile.py
+++ lesson3/08_smile.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
+
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
+def smile(point_x, point_y):
+    radius = 50
+    point = sd.get_point(point_x, point_y)
+    sd.circle(point, radius, sd.COLOR_YELLOW)
+    point = sd.get_point(point_x - 15, point_y + 20)
+    sd.circle(point, 5, sd.COLOR_YELLOW)
+    point = sd.get_point(point_x + 15, point_y + 20)
+    sd.circle(point, 5, sd.COLOR_YELLOW)
+    list_point = [sd.get_point(point_x - 25, point_y - 10), sd.get_point(point_x - 15, point_y - 20),
+                  sd.get_point(point_x + 15, point_y - 20),
+                  sd.get_point(point_x + 25, point_y - 10)]
+    sd.lines(list_point, sd.COLOR_YELLOW, False)
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+for _ in range(20):
+    smile(sd.random_number(50, 550), sd.random_number(50, 550))
+
+sd.pause()
