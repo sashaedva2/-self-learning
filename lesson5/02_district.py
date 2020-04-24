cb41d190af061b27eb39fde4898759b93d93bca0
Aleksandr <sashaedva@mail,ru>
2020-04-24 18:06:26 +0300

    программа вывода списка людй на районе

--- lesson5/02_district.py
+++ lesson5/02_district.py
@@ -4,9 +4,19 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
+from district.central_street.house1.room1 import folks as people1
+from district.central_street.house1.room2 import folks as people2
+from district.central_street.house2.room1 import folks as people3
+from district.central_street.house2.room2 import folks as people4
+from district.soviet_street.house1.room1 import folks as people11
+from district.soviet_street.house1.room2 import folks as people12
+from district.soviet_street.house2.room1 import folks as people13
+from district.soviet_street.house2.room2 import folks as people14
 
-# TODO здесь ваш код
+dots = ', '
+tags = [people1, people2, people3, people4, people11, people12, people13, people14]
+people = []
+for tag in tags:
+    people.extend(tag)
 
-
-
-
+print(dots.join(people))
