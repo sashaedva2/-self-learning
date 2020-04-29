a844e998872c59d427d8ddaab89dc580dd495276
Aleksandr <sashaedva@mail,ru>
2020-04-29 15:08:05 +0300

    Наркоманский снегопад организован, но при большом кол-ве снежинок, не успевает прорисовать

--- lesson6/02_snowfall_module.py
+++ lesson6/02_snowfall_module.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from random import randint
 
+colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+          sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE, sd.COLOR_WHITE)
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
 #  создать_снежинки(N) - создает N снежинок
@@ -14,21 +17,25 @@ import simple_draw as sd
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 from snowfall_engine import create_snowdrops, draw_snowdrop_with_color, move_snowdrops, number_to_bottom_of_screen, \
     del_snowdrop
-
-N = 20
+sd.resolution = (600, 800)
+N = 10
 # создать_снежинки(N)
 create_snowdrops(N)
 while True:
-    sd.start_drawing()
+    # sd.start_drawing()
     #  нарисовать_снежинки_цветом(color=sd.background_color)
     draw_snowdrop_with_color(color=sd.background_color)
     #  сдвинуть_снежинки()
     move_snowdrops()
     #  нарисовать_снежинки_цветом(color)
-    draw_snowdrop_with_color(color)
+    draw_snowdrop_with_color(color=colors[randint(0, 7)])
     #  если есть номера_достигших_низа_экрана() то
-    #       удалить_снежинки(номера)
+    #       удалить_снежинки()
     #       создать_снежинки(count)
+    if number_to_bottom_of_screen():
+        del_snowdrop()
+        create_snowdrops(randint(1, 2))
+    # sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
--- lesson6/snowfall_engine.py
+++ lesson6/snowfall_engine.py
@@ -1,23 +1,53 @@
+from random import randint
+import simple_draw as sd
+colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+          sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE, sd.COLOR_WHITE)
+global list_snowfall, list_ray, number
+list_snowfall = list()
+list_ray = list()
+numbers = list()
+
+
 def create_snowdrops(N):
     """Создание снежинок"""
-    pass
+    for _ in range(N):
+        list_snowfall.append([randint(0, 600), 790])
+    for _ in range(N):
+        list_ray.append(randint(10, 60))
 
 
 def draw_snowdrop_with_color(color):
     """Рисовать снежинки заданым цветом"""
-    pass
+    i = 0
+    for snowfall in list_snowfall:
+        point = sd.get_point(*snowfall)
+        sd.snowflake(center=point, length=list_ray[i], color=color)
+        i += 1
 
 
 def move_snowdrops():
     """Сдвиг снежинок"""
-    pass
+    for snowfall in list_snowfall:
+        snowfall[1] -= 10 + randint(-10, 20)
+        snowfall[0] += 10 + randint(-30, 10)
 
 
 def number_to_bottom_of_screen():
     """Список номеров упавших снежинок"""
-    pass
+    for i in range(len(list_snowfall)):
+        if list_snowfall[i][1] < 0:
+            numbers.append(i)
+    if len(numbers) > 0:
+        return True
+    else:
+        return False
 
 
-def del_snowdrop(numbers):
+def del_snowdrop():
     """Удаляет данные снежинки"""
-    pass
+    sd.clear_screen()
+    draw_snowdrop_with_color(color=colors[randint(0, 7)])
+    for num in numbers:
+        del list_snowfall[num]
+        del list_ray[num]
+    numbers.clear()
