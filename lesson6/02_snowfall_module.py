79223d5ac0f1eeb679c57c6187ed4e6daae43d3b
Aleksandr <sashaedva@mail,ru>
2020-04-29 03:16:39 +0300

    Заготовка под снегопад готова, далее необходимо реализовать функции Думаю все данные будут хранится  в пространтве имён импортируемого модуля

--- lesson6/02_snowfall_module.py
+++ lesson6/02_snowfall_module.py
@@ -12,12 +12,20 @@ import simple_draw as sd
 #
 # В текущем модуле реализовать главный цикл падения снежинок,
 # обращаясь ТОЛЬКО к функциям модуля snowfall
+from snowfall_engine import create_snowdrops, draw_snowdrop_with_color, move_snowdrops, number_to_bottom_of_screen, \
+    del_snowdrop
 
+N = 20
 # создать_снежинки(N)
+create_snowdrops(N)
 while True:
+    sd.start_drawing()
     #  нарисовать_снежинки_цветом(color=sd.background_color)
+    draw_snowdrop_with_color(color=sd.background_color)
     #  сдвинуть_снежинки()
+    move_snowdrops()
     #  нарисовать_снежинки_цветом(color)
+    draw_snowdrop_with_color(color)
     #  если есть номера_достигших_низа_экрана() то
     #       удалить_снежинки(номера)
     #       создать_снежинки(count)
---
+++ lesson6/snowfall_engine.py
@@ -0,0 +1,23 @@
+def create_snowdrops(N):
+    """Создание снежинок"""
+    pass
+
+
+def draw_snowdrop_with_color(color):
+    """Рисовать снежинки заданым цветом"""
+    pass
+
+
+def move_snowdrops():
+    """Сдвиг снежинок"""
+    pass
+
+
+def number_to_bottom_of_screen():
+    """Список номеров упавших снежинок"""
+    pass
+
+
+def del_snowdrop(numbers):
+    """Удаляет данные снежинки"""
+    pass
