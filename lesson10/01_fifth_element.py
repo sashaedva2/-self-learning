04720717ef659bcce7d18c6c61e73ab522d5f12a
Aleksandr <sashaedva@mail,ru>
2020-06-30 21:40:09 +0300

    Базовые навыки ловли исключени1 получены)

--- lesson10/01_fifth_element.py
+++ lesson10/01_fifth_element.py
@@ -3,18 +3,19 @@
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
 BRUCE_WILLIS = 42
-
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
-
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError:
+    print('невозможно преобразовать к числу. Написано не число на 5 месте строки.')
+except IndexError:
+    print('выход за границы списка. Необходимо как минимум пятизначную строку')
+except Exception as exc:
+    print("Неожиданная ошибка:", exc)
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
 # - остальные исключения
 # для каждого типа исключений написать на консоль соотв. сообщение
-
-
-
-
