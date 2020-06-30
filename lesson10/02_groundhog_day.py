f6cb9eeb01b28c46bfb4a1c9b42a08f7597a014c
Aleksandr <sashaedva@mail,ru>
2020-06-30 22:11:01 +0300

    Создание своих исключений для модуляции дня сурка.

--- lesson10/02_groundhog_day.py
+++ lesson10/02_groundhog_day.py
@@ -1,4 +1,30 @@
 # -*- coding: utf-8 -*-
+from random import randint
+
+
+class IamGodError(Exception):
+    pass
+
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
 
 # День сурка
 #
@@ -16,9 +42,46 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+def one_day():
+    try:
+        dice1 = randint(1, 7)
+        dice2 = randint(1, 13)
+        if dice2==1:
+            raise IamGodError('Я бог')
+        elif dice2==2:
+            raise DrunkError('Я напился')
+        elif dice2==3:
+            raise CarCrashError('Автомобильная авария')
+        elif dice2==4:
+            raise GluttonyError('Умер от еды')
+        elif dice2==5:
+            raise DepressionError('Депресссия')
+        elif dice2==6:
+            raise SuicideError('Суицид')
+        return dice1
+    except IamGodError:
+        print('Все будут так говорить если попадут в временную петлю')
+    except DrunkError:
+        print('Алкоголь зло')
+    except CarCrashError:
+        print('Дороги скользкие, осторожней')
+    except GluttonyError:
+        print('Еда конечно хорошо, но в меру')
+    except DepressionError:
+        print('Надо что-то делать, сменить род деятельности')
+    except SuicideError:
+        print('Не поможет смерть от временой петли')
+    return 0
+
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
-
-# TODO здесь ваш код
-
-# https://goo.gl/JnsDqu
+Day = 0
+Karma = 0
+while True:
+    Day += 1
+    print(f'День {Day}')
+    Karma += one_day()
+    print(f'Карма {Karma}')
+    if Karma >= ENLIGHTENMENT_CARMA_LEVEL:
+        print('Петля времени оборвана')
+        break
