67f8d94254387b7be94ef8721af28405fdd6dfb7
Aleksandr <sashaedva@mail,ru>
2020-07-04 01:17:28 +0300

    Генератор простых чисел тоже готов, но чувствуется, что тащить массив чисел необязательно.

--- lesson11/02_prime_numbers.py
+++ lesson11/02_prime_numbers.py
@@ -40,31 +40,39 @@ class PrimeNumbers:
             if self.i % prime == 0:
                 return self.__next__()
         else:
-            if self.i >self.n:
-                 raise StopIteration()
+            if self.i > self.n:
+                raise StopIteration()
             self.prime_numbers.append(self.i)
             return self.i
-          # признак того, что больше возвращать нечего
 
 
-prime_number_iterator = PrimeNumbers(n=10000)
-for number in prime_number_iterator:
-    print(number)
+# prime_number_iterator = PrimeNumbers(n=10)
+# for number in prime_number_iterator:
+#     print(number)
 
 
-# TODO после подтверждения части 1 преподователем, можно делать
 # Часть 2
 # Теперь нужно создать генератор, который выдает последовательность простых чисел до n
 # Распечатать все простые числа до 10000 в столбик
 
 
 def prime_numbers_generator(n):
-    pass
-    # TODO здесь ваш код
+    i = 1
+    prime_numbers = []
+    while True:
+        i += 1
+        for prime in prime_numbers:
+            if i % prime == 0:
+                break
+        else:
+            if i > n:
+                return
+            prime_numbers.append(i)
+            yield i
 
 
-# for number in prime_numbers_generator(n=10000):
-#     print(number)
+for number in prime_numbers_generator(n=100):
+    print(number)
 
 # Часть 3
 # Написать несколько функций-фильтров, которые выдает True, если число:
